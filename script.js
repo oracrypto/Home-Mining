7c8582c3c0e85337722b8206ade2e78cef716f0b
oracrypto <khalidaitahmed000@gmail.com>
2025-04-02 18:40:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,46 +4,73 @@ function login() {
 
            
             const keyMappings = {
-              "WXERBYUIMOPO": "https://crashpredr.blogspot.com/",
-              "VEXRCTVYBUULOYD": "https://cxrctgtor.blogspot.com/",
-              "XCVBNETRYFUYIBU": "https://ctvyhdictor.blogspot.com/",
-              "OILGJETRYCTUV": "https://bjuvhytor.blogspot.com/",
-              "ETRCYTVUIOPO": "https://gvhjor.blogspot.com/",
-              "WXERBYUIMOPO": "https://SERDTFYor.blogspot.com/",
-              "KNLJKBUYPO": "https://ERDTdtor.blogspot.com/",
-              "TRYCTGVHKBJV": "https://DRTFYrictor.blogspot.com/",
-              "NO69NFDD2MU": "https://ewrdf78.blogspot.com/",
-              "WXERBYUIMOPO": "https://crashpredr.blogspot.com/",
-              "VEXRCTVYBUULOYD": "https://cxrctgtor.blogspot.com/",
-              "XCVBNETRYFUYIBU": "https://ctvyhdictor.blogspot.com/",
-              "WERTDYNBNCVB": "https://ewrdf78.blogspot.com/",
-              "ETRCYTVUIOPO": "https://gvhjor.blogspot.com/",
-              "WXERBYUIMOPO": "https://SERDTFYor.blogspot.com/",
               
-              "TRYCTGVHKBJV": "https://DRTFYrictor.blogspot.com/",
-              "NO69NFDD2MU": "https://ewrdf78.blogspot.com/",
-              "WXERBYUIMOPO": "https://crashpredr.blogspot.com/",
-              "VETRCYTVUBOPJIU": "https://qwert445.blogspot.com/",
-              "XCVBNETRYFUYIBU": "https://ctvyhdictor.blogspot.com/",
+              
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                         
-              "OILGJETRYCTUV": "https://bjuvhytor.blogspot.com/",
-              "ETRCYTVUIOPO": "https://gvhjor.blogspot.com/",
-              "WJF74BIF9W2TR": "https://oracrypto.github.io/cryptoruit/",
-              "KNLJKBUYPO": "https://ERDTdtor.blogspot.com/",
-              "TRYCTGVHKBJV": "https://DRTFYrictor.blogspot.com/",
-              "ESRDTFYG6R89": "https://oracrypto.github.io/cryptoruit/",
-                        
-              "WXERBYUIMOPO": "https://crashpredr.blogspot.com/",
-              "VEXRCTVYBUULOYD": "https://cxrctgtor.blogspot.com/",
-              "XCVBNETRYFUYIBU": "https://oracrypto.github.io/cryptoruit/",
-              "AOLKYYBUIKUE": "https://crashpredictor.blogspot.com/",
-              "ETRCYTVUIOPO": "https://gvhjor.blogspot.com/",
-                        
-              "WXERBYUIMOPO": "https://oracrypto.github.io/cryptoruit/",
-              "KNLJKBUYPO": "https://ERDTdtor.blogspot.com/",
               "TRYCTGVHKBJV": "https://DRTFYrictor.blogspot.com/",
               "A3V5LFKFCN2": "https://qwert445.blogspot.com/",
-              "NO69NFDD2MU": "https://oracrypto.github.io/cryptoruit/"
+              "KRVUH3FTYKR2": "https://oracrypto.github.io/cryptoruit/"
                
             };
 
