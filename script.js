aa87da9d4b0291cb439e22cf56bc0feb1c6d910b
oracrypto <khalidaitahmed000@gmail.com>
2025-04-07 19:40:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -67,12 +67,7 @@ function login() {
 
 
 
-              "WVBB5Y9NW5Y12": "https://oracrypto.github.io/2Block/",
-              "BVZCNUAET4TG8": "https://oracrypto.github.io/4Block/",
-              "VSEUIONT89Y6Q": "https://oracrypto.github.io/cryptoruit/",        
-              "5Y8KMVBOEHVY9": "https://oracrypto.github.io/2Block/",
-              "XWEZCR6D4JIX93": "https://oracrypto.github.io/4Block/",
-              "KRVUH3FTYKR2": "https://oracrypto.github.io/cryptoruit/"
+              
                
             };
 
