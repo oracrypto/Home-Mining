8048c7ffeb85406e2c604eba8acc11516ef76bea
oracrypto <khalidaitahmed000@gmail.com>
2025-04-11 02:07:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,204 @@
-function login() {
-            let key = document.getElementById("activationKey").value;
-            const errorMessage = document.getElementById("error-message");
-
-           
-            const keyMappings = {
+function login() {let key = document.getElementById("activationKey").value;
+ const errorMessage = document.getElementById("error-message");  const keyMappings = {
             "XRDCTGVYBUHJNI": "https://oracrypto.COM",
             "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
             "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "www.ertyu.rt",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-              "WVBB5Y9NW5Y12": "https://oracrypto.github.io/2Block/",
-              "BVZCNUAET4TG8": "https://oracrypto.github.io/4Block/",
-              "VSEUIONT89Y6Q": "https://oracrypto.github.io/cryptoruit/",        
-              "5Y8KMVBOEHVY9": "https://oracrypto.github.io/2Block/",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "www.txrytuvyiu.ty",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+       "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"WVBB5Y9NW5Y12": "https://oracrypto.github.io/2Block/",
+           "BVZCNUAET4TG8": "https://oracrypto.github.io/4Block/",
+  "VSEUIONT89Y6Q": "https://oracrypto.github.io/cryptoruit/",    
+           "AXSERBGYU9GNH": "XECRTVBYUNIM",    
+"ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+           "5Y8KMVBOEHVY9": "https://oracrypto.github.io/2Block/",
+ "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",    
+"ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
               "XWEZCR6D4JIX93": "https://oracrypto.github.io/4Block/",
-              "KRVUH3FTYKR2": "https://oracrypto.github.io/cryptoruit/"
+"KRVUH3FTYKR2": "https://oracrypto.github.io/cryptoruit/",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+     "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+                  "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+                         "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+             "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+             
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",    
+             "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",    
+             "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
 
 
 
 
 
 
+
+             
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM"
+     "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+             "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+             
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",    
+             "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+     "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+                  "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM",
+"XRDCTGVYBUHJNI": "https://oracrypto.COM",
+            "ZXS4CD5FVU6BG": "TBYUNMILCVGBHJN",
+            "AXSERBGYU9GNH": "XECRTVBYUNIM"
 
 
 
