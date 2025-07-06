547143ef1ae2d5b72148c97637607afd56931544
oracrypto <khalidaitahmed000@gmail.com>
2025-07-06 14:22:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,57 @@
 function login() {let key = document.getElementById("activationKey").value;
  const errorMessage = document.getElementById("error-message");  const keyMappings = {
-"AXSERBGYU9GNH": "XECRTVBYUNIM",   "ZXS4CD5FVUwerG": "TBYUNMILCVGBHJN",   "AXSERBGYU9GNH": "XECRTVBYUNIM",
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
  "567VCMWEOHWIG": "https://oracrypto.github.io/1b/", "BOSVRTTHB8CD": "https://oracrypto.github.io/2b/", "VCI7TGH29XHQGTE": "https://oracrypto.github.io/4b/",
- "VC86HJ73BWUGDH": "https://oracrypto.github.io/6b/", "BYUNMILCVGBHJN": "https://oracrypto.github.io/f233/", "https://oracrypto.github.io/WE/": "TBYUNMILCVGBHJN",
+ "VC86HJ73BWUGDH": "https://oracrypto.github.io/6b/",
 
-            "AXSERBGYU9GNH": "https://oracrypto.github.io/WE/"
 
+
+  
+  "AR52F1C9HFECHRX": "https://oracrypto.github.io/1b/", "BX74SDYI9SJEFWID": "https://oracrypto.github.io/2b/",
+"BVNCXIU6336XBDJE": "https://oracrypto.github.io/4b/", "VCNSXJU3YCHD83": "https://oracrypto.github.io/6b/",
+            "C4479UHDYHJDGJHJHK": "https://oracrypto.github.io/WE/"
+
+
+
+
+
+
+
+
+
+
+
+  
  };
-
+<!--WW-->
             
             if (keyMappings[key]) {
                 window.location.href = keyMappings[key]; 
