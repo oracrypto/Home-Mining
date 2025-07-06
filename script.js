1b93ae39e8d636d35adc90081d53376b6d9bd1a3
oracrypto <khalidaitahmed000@gmail.com>
2025-07-06 14:27:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,28 +12,14 @@ function login() {let key = document.getElementById("activationKey").value;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//old keys 
 
   
  "567VCMWEOHWIG": "https://oracrypto.github.io/1b/", "BOSVRTTHB8CD": "https://oracrypto.github.io/2b/", "VCI7TGH29XHQGTE": "https://oracrypto.github.io/4b/",
  "VC86HJ73BWUGDH": "https://oracrypto.github.io/6b/",
 
 
-
+//new keys
   
   "AR52F1C9HFECHRX": "https://oracrypto.github.io/1b/", "BX74SDYI9SJEFWID": "https://oracrypto.github.io/2b/",
 "BVNCXIU6336XBDJE": "https://oracrypto.github.io/4b/", "VCNSXJU3YCHD83": "https://oracrypto.github.io/6b/",
