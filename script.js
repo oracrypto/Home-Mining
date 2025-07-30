344df7ef8ca9edd96a6b2bbacf69bea49ce99651
oracrypto <khalidaitahmed000@gmail.com>
2025-07-30 01:12:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,15 @@
 //old keys 
 
   
- "FEUDT26GK59XGKD": "https://oracrypto.github.io/1b/", "CJYZJ47JOECAM": "https://oracrypto.github.io/2b/", "NCOAY36PDNW3H": "https://oracrypto.github.io/4b/",
- "VC86HJ73BWUGDH": "https://oracrypto.github.io/6b/",
+ "": "https://oracrypto.github.io//", "": "https://oracrypto.github.io//", "": "https://oracrypto.github.io//",
+ "": "https://oracrypto.github.io//",
 
 
 //new keys
   
-  "AR52F1C9HFECHRX": "https://oracrypto.github.io/1b/", "BX74SDYI9SJEFWID": "https://oracrypto.github.io/2b/",
-"BVNCXIU6336XBDJE": "https://oracrypto.github.io/4b/", "XHRVYECS583VU": "https://oracrypto.github.io/6b/",
-            "C4479UHDYHJDGJHJHK": "https://oracrypto.github.io/WE/",
+  "": "https://oracrypto.github.io//", "": "https://oracrypto.github.io//",
+"": "https://oracrypto.github.io//", "": "https://oracrypto.github.io//",
+            "": "https://oracrypto.github.io/WE/",
 
 
 
@@ -22,7 +22,7 @@
 
 
   
-            "AXSERBGYU9GNH": "www.esdrtfgyuhj.site"
+            "": ""
 
  };
 
