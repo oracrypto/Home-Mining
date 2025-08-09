662fb76a9c0e3a5c8a6d51f47d864bc074a67a9a
oracrypto <khalidaitahmed000@gmail.com>
2025-08-09 02:51:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
   function login() {let key = document.getElementById("activationKey").value;
  const errorMessage = document.getElementById("error-message");  const keyMappings = {
 //demo keys
- "DEMO-KEY2025-PK19": "https://demo-key-2025.blogspot.com/", "DEMO-KEY2025-BD6D": "https://demo-key-2025.blogspot.com/",
+ 
 
    
 //old keys 
 
   
- "MDO62BXL92NXO": "https://oracrypto.github.io/1b/", "NDOS52NCOS73H": "https://oracrypto.github.io/2b/", "NDO63NFPW10DK": "https://oracrypto.github.io/4b/",
+"DEMO-KEY2025-PK19": "https://demo-key-2025.blogspot.com/", "DEMO-KEY2025-BD6D": "https://demo-key-2025.blogspot.com/",
+"MDO62BXL92NXO": "https://oracrypto.github.io/1b/", "NDOS52NCOS73H": "https://oracrypto.github.io/2b/", "NDO63NFPW10DK": "https://oracrypto.github.io/4b/",
  "BXO38NXOSWPP20": "https://oracrypto.github.io/6b/",
 
 
